d444b8b9ae1e258889e6de27ec25f3505866abc7
Jazeel Meerasah <jazeel_18@hotmail.com>
2023-07-16 11:02:55 +0800

    initial commit with lambda only

---
+++ lambda.tf
@@ -0,0 +1,31 @@
+data "aws_iam_policy_document" "lambda_assume_role_policy" {
+  statement {
+    effect  = "Allow"
+    actions = ["sts:AssumeRole"]
+    principals {
+      type        = "Service"
+      identifiers = ["lambda.amazonaws.com"]
+    }
+  }
+}
+
+resource "aws_iam_role" "lambda_role" {
+  name               = "lambda-hw-role"
+  assume_role_policy = data.aws_iam_policy_document.lambda_assume_role_policy.json
+}
+
+data "archive_file" "python_lambda_package" {
+  type        = "zip"
+  source_file = "${path.module}/lambda_function.py"
+  output_path = "nametest.zip"
+}
+
+resource "aws_lambda_function" "test_lambda_function" {
+  function_name    = "lambdaTest"
+  filename         = "nametest.zip"
+  source_code_hash = data.archive_file.python_lambda_package.output_base64sha256
+  role             = aws_iam_role.lambda_role.arn
+  runtime          = "python3.9"
+  handler          = "lambda_function.lambda_handler"
+  timeout          = 10
+}

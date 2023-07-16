7a98ac74132d8d22dd7960a967a497f4da8a5ab1
Jazeel Meerasah <jazeel_18@hotmail.com>
2023-07-16 11:18:46 +0800

    Add apigw

--- lambda.tf
+++ lambda.tf
@@ -17,12 +17,12 @@ resource "aws_iam_role" "lambda_role" {
 data "archive_file" "python_lambda_package" {
   type        = "zip"
   source_file = "${path.module}/lambda_function.py"
-  output_path = "nametest.zip"
+  output_path = "lambda.zip"
 }
 
-resource "aws_lambda_function" "test_lambda_function" {
-  function_name    = "lambdaTest"
-  filename         = "nametest.zip"
+resource "aws_lambda_function" "hello_lambda" {
+  function_name    = "lambda-apigw"
+  filename         = "lambda.zip"
   source_code_hash = data.archive_file.python_lambda_package.output_base64sha256
   role             = aws_iam_role.lambda_role.arn
   runtime          = "python3.9"

6fc36a62f34bebd272c283ff8abc865baf639bf1
Jazeel Meerasah <jazeel_18@hotmail.com>
2023-07-19 20:31:06 +0800

    Add lambda exec role

--- lambda.tf
+++ lambda.tf
@@ -9,6 +9,11 @@ data "aws_iam_policy_document" "lambda_assume_role_policy" {
   }
 }
 
+resource "aws_iam_role_policy_attachment" "terraform_lambda_policy" {
+  role       = "${aws_iam_role.lambda_role.name}"
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+}
+
 resource "aws_iam_role" "lambda_role" {
   name               = "${var.your_name}-lambda-hw-role"
   assume_role_policy = data.aws_iam_policy_document.lambda_assume_role_policy.json
